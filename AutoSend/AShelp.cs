0613aa4c1418acfd2fb794fda688ca0ff37c9ff3
fbkc <739650808@qq.com>
2019-01-14 17:37:28 +0800

    1.14.0.1

--- AutoSend/AShelp.cs
+++ AutoSend/AShelp.cs
@@ -12,6 +12,25 @@ namespace AutoSend
 {
     public static class AShelp
     {
+        /// <summary>
+        /// GB2312转换成UTF8
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static string gb2312_utf8(string text)
+        {
+            //声明字符集   
+            System.Text.Encoding utf8, gb2312;
+            //gb2312   
+            gb2312 = System.Text.Encoding.GetEncoding("gb2312");
+            //utf8   
+            utf8 = System.Text.Encoding.GetEncoding("utf-8");
+            byte[] gb;
+            gb = gb2312.GetBytes(text);
+            gb = System.Text.Encoding.Convert(gb2312, utf8, gb);
+            //返回转换后的字符   
+            return utf8.GetString(gb);
+        }
         public static string[] RandomStrings(string[] str, int count)
         {
             if (str.Length > 0)
